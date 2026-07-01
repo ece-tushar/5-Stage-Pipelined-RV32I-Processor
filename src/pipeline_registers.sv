fae0f2724317383c58d0715f775efef72a96e7d1
ece-tushar <indigotushar@gmail.com>
2026-07-01 21:21:12 +0530

    Update pipeline_registers.sv

--- src/pipeline_registers.sv
+++ src/pipeline_registers.sv
@@ -95,3 +95,106 @@ assign Imm_out = ID_EX.IMM;
 assign Ctrl_out = ID_EX.CTRL;             
       
 endmodule
+
+
+
+module pipe_EX_MEM #(
+    parameter ADDR_WIDTH     = 8,
+    parameter REG_ADDR_WIDTH = 5,
+    parameter CTRL_WORD_WIDTH = 9,
+    parameter DATA_WIDTH     = 32
+    )( input logic Clk,Rst,Flush,
+    
+       input logic [ADDR_WIDTH-1:0] PC4_in,
+       input logic [REG_ADDR_WIDTH-1:0] Rd_addr_in,
+       input logic [DATA_WIDTH-1:0] Rs2_in,
+       input logic [DATA_WIDTH-1:0] ALU_in,
+       input logic [CTRL_WORD_WIDTH-1:0] Ctrl_in,
+        
+       output logic [ADDR_WIDTH-1:0] PC4_out,
+       output logic [REG_ADDR_WIDTH-1:0] Rd_addr_out,
+       output logic [DATA_WIDTH-1:0] Rs2_out,
+       output logic [DATA_WIDTH-1:0] ALU_out,
+       output logic [CTRL_WORD_WIDTH-1:0] Ctrl_out
+       );
+       
+   struct packed {
+        logic [ADDR_WIDTH-1:0] PC4;
+        logic [REG_ADDR_WIDTH-1:0] RD;
+        logic [DATA_WIDTH-1:0] RS2;
+        logic [DATA_WIDTH-1:0] ALU;
+        logic [CTRL_WORD_WIDTH-1:0] CTRL;
+        } EX_MEM;
+   
+   always_ff @ (posedge Clk) begin
+        if (Rst || Flush) begin
+            EX_MEM <= '0;
+            end
+
+        else begin
+            EX_MEM.PC4   <= PC4_in;
+            EX_MEM.RD    <= Rd_addr_in;
+            EX_MEM.RS2    <= Rs2_in;
+            EX_MEM.ALU   <= ALU_in;
+            EX_MEM.CTRL  <= Ctrl_in;
+            end
+   end
+
+assign PC4_out = EX_MEM.PC4; 
+assign Rd_addr_out = EX_MEM.RD;             
+assign Rs2_out = EX_MEM.RS2;             
+assign ALU_out = EX_MEM.ALU;             
+assign Ctrl_out = EX_MEM.CTRL;             
+      
+endmodule
+
+
+module pipe_MEM_WB #(
+    parameter ADDR_WIDTH     = 8,
+    parameter REG_ADDR_WIDTH = 5,
+    parameter CTRL_WORD_WIDTH = 3,
+    parameter DATA_WIDTH     = 32
+    )( input logic Clk,Rst,Flush,
+    
+       input logic [ADDR_WIDTH-1:0] PC4_in,
+       input logic [REG_ADDR_WIDTH-1:0] Rd_addr_in,
+       input logic [DATA_WIDTH-1:0] ALU_in,
+       input logic [DATA_WIDTH-1:0] DM_in,
+       input logic [CTRL_WORD_WIDTH-1:0] Ctrl_in,
+        
+       output logic [ADDR_WIDTH-1:0] PC4_out,
+       output logic [REG_ADDR_WIDTH-1:0] Rd_addr_out,
+       output logic [DATA_WIDTH-1:0] ALU_out,
+       output logic [DATA_WIDTH-1:0] DM_out,
+       output logic [CTRL_WORD_WIDTH-1:0] Ctrl_out
+       );
+       
+   struct packed {
+        logic [ADDR_WIDTH-1:0] PC4;
+        logic [REG_ADDR_WIDTH-1:0] RD;
+        logic [DATA_WIDTH-1:0] DM;
+        logic [DATA_WIDTH-1:0] ALU;
+        logic [CTRL_WORD_WIDTH-1:0] CTRL;
+        } MEM_WB;
+   
+   always_ff @ (posedge Clk) begin
+        if (Rst || Flush) begin
+            MEM_WB <= '0;
+            end
+
+        else begin
+            MEM_WB.PC4   <= PC4_in;
+            MEM_WB.RD    <= Rd_addr_in;
+            MEM_WB.DM    <= DM_in;
+            MEM_WB.ALU   <= ALU_in;
+            MEM_WB.CTRL  <= Ctrl_in;
+            end
+   end
+
+assign PC4_out = MEM_WB.PC4; 
+assign Rd_addr_out = MEM_WB.RD;             
+assign DM_out = MEM_WB.DM;             
+assign ALU_out = MEM_WB.ALU;             
+assign Ctrl_out = MEM_WB.CTRL;             
+      
+endmodule
