2a448c0ad5b675de911b5571a5838f37a330e5e5
ece-tushar <indigotushar@gmail.com>
2026-07-02 22:37:22 +0530

    Update pipe_tb.sv

--- sim/pipe_tb.sv
+++ sim/pipe_tb.sv
@@ -1,110 +1,135 @@
-module pipe_tb;
+`timescale 1ns/1ps
 
-parameter ADDR_WIDTH = 8;
-parameter DATA_WIDTH = 32;
+module dptb;
 
-logic  Rst,Clk;
-logic [ADDR_WIDTH-1:0] PC_in;
-logic [DATA_WIDTH-1:0] IM_in;
-logic [ADDR_WIDTH-1:0] PC_out;
-logic [DATA_WIDTH-1:0] IM_out;
+reg Clk, Rst;
+wire [31:0] DataOut;
 
-logic [ADDR_WIDTH-1:0] arrpc [0:9];
-logic [ADDR_WIDTH-1:0] arrim [0:9];
-logic [DATA_WIDTH-1:0] checkpc [0:9];
-logic [DATA_WIDTH-1:0] checkim [0:9];
+DataPath uut (.Clk(Clk),.Rst(Rst),.DataOut(DataOut));
+
+// probing stalling issues. 
+wire [7:0] PCOut = uut.PC_RAddr;
+wire  Stall = uut.StallReq;
+wire  PCWen = uut.PC_WEn;
+wire  flush_ID = uut.ID_EX_Flush;
+wire  [31:0] IF_ID = uut.ID_IM_Instr;
+wire  [31:0] ID_EX = uut.EX_IM_Instr;
+
+
 
 initial begin
-Rst = 0;
 Clk = 1;
-arrpc = '{1,2,3,4,5,6,7,8,9,10};
-arrim = '{11,12,13,14,15,16,17,18,19,20};
-checkpc = '{default:0};
-checkim = '{default:0};
+Rst = 1; 
 end
-
-pipe_IF_ID uut (.Rst(Rst),.Clk(Clk),
-                .PC_in(PC_in),.IM_in(IM_in),
-                .PC_out(PC_out),.IM_out(IM_out));
-                
-task reset;
-begin
-    @(negedge Clk)
-        Rst=1;
-    
-    repeat (2) @(negedge Clk);
-    
-    @(negedge Clk) 
-        Rst = 0;
-end
-endtask
-
-task write_store;
-//integer i;
-begin
-    foreach(arrpc[i]) begin
-        @(negedge Clk) begin
-            PC_in = arrpc[i];
-            IM_in = arrim[i];
-            end
-        @(negedge Clk) begin
-            checkpc[i] = PC_out;
-            checkim[i] = IM_out;
-            end
-     end
-end
-endtask
-
-task check;
-    integer pc, im;
-
-    begin
-        pc = 0;
-        im = 0;
-
-        foreach (arrpc[i]) begin
-
-            if (arrpc[i] == checkpc[i]) begin
-                $display("PASS : PC[%0d] Expected = %0h, Got = %0h",
-                         i, arrpc[i], checkpc[i]);
-                pc++;
-            end
-            else begin
-                $display("FAIL : PC[%0d] Expected = %0h, Got = %0h",
-                         i, arrpc[i], checkpc[i]);
-            end
-
-            if (arrim[i] == checkim[i]) begin
-                $display("PASS : IM[%0d] Expected = %0h, Got = %0h",
-                         i, arrim[i], checkim[i]);
-                im++;
-            end
-            else begin
-                $display("FAIL : IM[%0d] Expected = %0h, Got = %0h",
-                         i, arrim[i], checkim[i]);
-            end
-
-        end
-
-        $display("----------------------------------------");
-        $display("PC  : %0d/%0d Passed", pc, $size(arrpc));
-        $display("IM  : %0d/%0d Passed", im, $size(arrim));
-
-        if ((pc == $size(arrpc)) && (im == $size(arrim)))
-            $display("******** TEST PASSED ********");
-        else
-            $display("******** TEST FAILED ********");
-
-    end
-endtask
 
 always #5 Clk = ~Clk;
 
-initial begin
-    reset();
-    write_store();
-    check();
+task reset_PC_RB;
+begin
+    @(negedge Clk) begin
+        Rst = 1;
+        end
+    repeat (2) @(negedge Clk);
 end
+endtask
 
+task write_program;
+reg [31:0] prog [0:63];
+integer i;
+begin
+    $readmemh("program.mem",prog);
+    for (i = 0; i < 64; i = i + 1) begin
+        uut.IM.RAM0.mem[i] = prog[i][7:0];
+        uut.IM.RAM1.mem[i] = prog[i][15:8];
+        uut.IM.RAM2.mem[i] = prog[i][23:16];
+        uut.IM.RAM3.mem[i] = prog[i][31:24];
+    end
+end
+endtask
 
+task write_data;
+reg [31:0] data [0:63];
+integer i;
+begin
+    $readmemh("data.mem",data);
+    for (i = 0; i < 64; i = i + 1) begin
+        uut.DM.RAM0.mem[i] = data[i][7:0];
+        uut.DM.RAM1.mem[i] = data[i][15:8];
+        uut.DM.RAM2.mem[i] = data[i][23:16];
+        uut.DM.RAM3.mem[i] = data[i][31:24];
+    end
+end
+endtask
+
+task drop_reset_PC_RB;
+begin
+    @(negedge Clk) begin
+        Rst = 0;
+        end
+end
+endtask 
+
+//task check_RB;
+//reg [31:0] target [0:31];
+//integer i;
+//integer pass, fail;
+//begin
+//pass = 0;
+//fail = 0;
+//    $readmemh("target.mem",target);
+
+//    for (i=0;i<32;i=i+1) begin
+//        if (uut.RB.mem[i] == target[i]) begin
+//                $display("PASS x%0d = %0h       |       Expected = %0h",i,uut.RB.mem[i],target[i]);
+//                pass = pass + 1;
+//            end
+//        else begin
+//                $display("FAIL x%0d = %0h       |       Expected = %0h",i,uut.RB.mem[i],target[i]);
+//                fail = fail + 1;
+//            end
+//    end
+//    $display("==============================================");
+//    $display("PASS = %0d        |       FAIL = %0d",pass,fail);
+//    $display("==============================================");
+
+//end
+//endtask 
+
+task check_DM;
+reg [31:0] target [0:63];
+integer i;
+integer pass, fail;
+begin
+pass = 0;
+fail = 0;
+    $readmemh("target.mem",target);
+
+    for (i=0;i<64;i=i+1) begin
+        if ({uut.DM.RAM3.mem[i],uut.DM.RAM2.mem[i],uut.DM.RAM1.mem[i],uut.DM.RAM0.mem[i]} == target[i]) begin
+                $display("PASS WA%0d = %h       |       Expected = %h",i,{uut.DM.RAM3.mem[i],uut.DM.RAM2.mem[i],uut.DM.RAM1.mem[i],uut.DM.RAM0.mem[i]},target[i]);
+                pass = pass + 1;
+            end
+        else begin
+                $display("FAIL WA%0d = %h       |       Expected = %h",i,{uut.DM.RAM3.mem[i],uut.DM.RAM2.mem[i],uut.DM.RAM1.mem[i],uut.DM.RAM0.mem[i]},target[i]);
+                fail = fail + 1;
+            end
+    end
+    $display("======================================================");
+    $display("PASS = %0d                  |       FAIL = %0d",pass,fail);
+    $display("======================================================");
+
+end
+endtask 
+
+integer k;
+
+initial begin
+    write_data();
+    write_program();      // LIGHTS!
+    reset_PC_RB();        // CAMERA!!
+    drop_reset_PC_RB();   // ACTION!!!
+    #5000;
+    check_DM();
+    $finish;
+    end
 endmodule
